c8bd1787726bca100af34e39109e3dd376711cc5
Robert Koch <r.tobiaskoch@gmail.com>
2025-06-03 16:22:27 -0600

    before adding optional columns to culex_clean_sheet

--- utils/fun_culex_sheet_clean.R
+++ utils/fun_culex_sheet_clean.R
@@ -11,7 +11,7 @@
 #' @return A cleaned and standardized data frame with columns:
 #' `trap_id`, `trap_date`, `year`, `week`, `zone`, `spp`, `method`, and `total`.
 #'
-#' @details This function depends on `parse_flexible_date()`, which must be defined elsewhere
+#' @details This function depends on `parse_flexible_date()` and `col_rename()` which must be defined elsewhere
 #' in the package or user's environment.
 #'
 #' @examples
@@ -25,29 +25,38 @@
 #' @importFrom lubridate year week
 #' @export
 culex_sheet_clean <- function(df) {
-  required_cols <- c("trap_name", "date_trap_set", 
-                     "mosquito_species","trap_type", 
-                     "mosquito_count", "zone")
   
-  missing_cols <- setdiff(required_cols, names(df))
+   
+  col_key <- list(
+    trap_id   = c("trap_name"),
+    trap_date = c("date_trap_set"),
+    spp       = c("mosquito_species"),
+    method    = c("trap_type"),
+    total     = c("mosquito_count"),
+    zone      = c("zone")
+  )
+
+  missing_cols <- setdiff(names(col_key), names(df))
   if (length(missing_cols) > 0) {
-    stop("Missing required columns: ", paste(missing_cols, collapse = ", "))
+    Cat(" Warning. Missing columns: ", paste(missing_cols, collapse = ", "))
   }
+  
+  df = key_rename(df, col_key, drop = T)
   
   df %>%
     dplyr::mutate(dplyr::across(where(is.character), trimws)) %>% #remove any whitespace
-    dplyr::mutate(trap_date = purrr::map_chr(date_trap_set, ~ as.character(parse_flexible_date(.x)))) %>% #reformat dates using the utils/fun_parse_flexible_date
+    dplyr::mutate(trap_date = purrr::map_chr(trap_date, ~ as.character(parse_flexible_date(.x)))) %>% #reformat dates using the utils/fun_parse_flexible_date
     dplyr::mutate(trap_date = as.Date(trap_date)) %>%
-    dplyr::transmute(
+    dplyr::mutate(
       trap_id = toupper(trap_name),
       trap_date = trap_date,
       year = lubridate::year(trap_date),
       week = lubridate::week(trap_date),
       zone = zone,
       spp = dplyr::case_when(
-        mosquito_species == "Culex tarsalis" ~ "Tarsalis",
-        mosquito_species == "Culex pipiens" ~ "Pipiens",
-        TRUE ~ mosquito_species
+        spp == "Culex tarsalis" ~ "Tarsalis",
+        spp == "Culex pipiens" ~ "Pipiens",
+        TRUE ~ spp
       ),
    #   trap_type = trap_type,
       method = case_when(stringr::str_detect(tolower(trap_id), "gr") ~ "G",
